7229c65913fc1d63c5cabd52dfb71c1f41d744b4
Rob Sletten <rsletten@users.noreply.github.com>
2021-03-18 12:24:20 -0400

    Update kubeinit.sh

--- kubeinit.sh
+++ kubeinit.sh
@@ -1,26 +1,26 @@
+kubeadm init --pod-network-cidr=10.0.0.0/16 --service-dns-domain=k8s.rsletten.com
 
-
-Your Kubernetes control-plane has initialized successfully!
-
-To start using your cluster, you need to run the following as a regular user:
-
-  mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-Alternatively, if you are the root user, you can run:
-
-  export KUBECONFIG=/etc/kubernetes/admin.conf
-
-You should now deploy a pod network to the cluster.
-Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
-  https://kubernetes.io/docs/concepts/cluster-administration/addons/
-
-Then you can join any number of worker nodes by running the following on each as root:
-
-kubeadm join 192.168.1.63:6443 --token 2pyun5.0niww8p8l72cwdlp \
-    --discovery-token-ca-cert-hash sha256:7c4ad395970f2e1bbe4597bf0abf7fa2c89facc1a3c47f8ce554e44e1d77dfa4
-
+# Your Kubernetes control-plane has initialized successfully!
+#
+# To start using your cluster, you need to run the following as a regular user:
+#
+#  mkdir -p $HOME/.kube
+#  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#
+# Alternatively, if you are the root user, you can run:
+#
+#  export KUBECONFIG=/etc/kubernetes/admin.conf
+#
+# You should now deploy a pod network to the cluster.
+# Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
+# https://kubernetes.io/docs/concepts/cluster-administration/addons/
+#
+# Then you can join any number of worker nodes by running the following on each as root:
+#
+# kubeadm join 192.168.1.63:6443 --token 2pyun5.0niww8p8l72cwdlp \
+#    --discovery-token-ca-cert-hash sha256:7c4ad395970f2e1bbe4597bf0abf7fa2c89facc1a3c47f8ce554e44e1d77dfa4
+#
 
 # calico
 wget https://docs.projectcalico.org/manifests/tigera-operator.yaml
@@ -38,6 +38,7 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: f
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
